b90207e331fd3d517a7994af914ef6dde599da60
moredream <moredream7@gmail.com>
2014-02-13 13:58:49 +0800

    design 01

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,15 @@
 class WelcomeController < ApplicationController
 
+  respond_to :html, :json
+
   def index
-
-
-
+    respond_with(labs)
   end
+
+
+private
+  def labs
+    @labs ||= Lab.order(:name)
+  end
+
 end
