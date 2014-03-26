64de8f52f5b8e73649d882d4f9e7a15d984fa696
moredream <moredream7@gmail.com>
2014-03-26 10:54:01 +0800

    Tagging 1.0

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,6 @@ class EventsController < ApplicationController
 
 	def index
 
-
 	end
 
 	def search
@@ -17,21 +16,20 @@ class EventsController < ApplicationController
 	end
 
 	def create
-	@event = current_user.articles.build(event_params)
-	if @event.save
-	  redirect_to @event , notice: 'Program was successfully created'
-	else
-	  render 'new'
-	end
+		@event = current_user.articles.build(event_params)
+		if @event.save
+		  redirect_to @event , notice: 'Program was successfully created'
+		else
+		  render 'new'
+		end
 	end
 
 	def show
-	@commentable = @event
-	@comment = Comment.new
-	@attachable = @event
-	@photos = @attachable.photos
-	@voter = current_user
-
+		@commentable = @event
+		@comment = Comment.new
+		@attachable = @event
+		@photos = @attachable.photos
+		@voter = current_user
 	end
 
 	def edit
@@ -47,30 +45,30 @@ class EventsController < ApplicationController
 	end
 
 	def destroy
-	@event.destroy
-	redirect_to articles_url
+		@event.destroy
+		redirect_to events_url
 	end
 
 	def upvote
-	respond_to do |format|
-	  if @event.liked_by current_user
-	    format.html { redirect_to @event, notice: "Thanks for vote!" }
-	    format.js
-	  else
-	    render @event
-	  end
-	end
+		respond_to do |format|
+		  if @event.liked_by current_user
+		    format.html { redirect_to @event, notice: "Thanks for vote!" }
+		    format.js
+		  else
+		    render @event
+		  end
+		end
 	end
 
 	def downvote
-	respond_to do |format|
-	  if  @event.downvote_from current_user
-	    format.html { redirect_to @event, notice: "Thanks for vote!" }
-	    format.js
-	  else
-	    render @event
-	  end
-	end
+		respond_to do |format|
+		  if  @event.downvote_from current_user
+		    format.html { redirect_to @event, notice: "Thanks for vote!" }
+		    format.js
+		  else
+		    render @event
+		  end
+		end
 	end
 
 	private
@@ -89,16 +87,14 @@ class EventsController < ApplicationController
 
 	helper_method :offers
 
-
 	def events
-	if params[:tag]
-	  @events = Event.tagged_with(params[:tag]).page(params[:page]).per(2)
-	elsif params[:q] || params[:tag_id]
-	  @events ||= Event.search_tags(params[:q], params[:tag_id]).page(params[:page]).per(2)
-	else
-	  @events ||= Event.trends('100').page(params[:page]).per(2)
-	end
-
+		if params[:tag]
+		  @events = Event.tagged_with(params[:tag]).page(params[:page]).per(2)
+		elsif params[:q] || params[:tag_id]
+		  @events ||= Event.search_tags(params[:q], params[:tag_id]).page(params[:page]).per(2)
+		else
+		  @events ||= Event.trends('100').page(params[:page]).per(2)
+		end
 	end
 
 	helper_method :events
